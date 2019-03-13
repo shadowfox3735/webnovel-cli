cda5eb9b27fec1e54b41973e4b5df85c5f838a40
shadowfox3735 <normanpan180@gmail.com>
2019-03-13 00:14:39 +0000

    added methods to Webnovel class

--- lib/webnovel/webnovel.rb
+++ lib/webnovel/webnovel.rb
@@ -1,3 +1,22 @@
 class Webnovel
   attr_accessor :name, :genres, :latest_chapter
+  @@all = []
+  
+  def initialize(name, genres, latest_chapter)
+    @name = name
+    @genres = genres
+    @latest_chapter = latest_chapter
+  end
+  
+  def save
+    @@all << self
+  end
+  
+  def self.all
+    @@all
+  end
+  
+  def self.destroyall
+    @@all.clear
+  end
 end
